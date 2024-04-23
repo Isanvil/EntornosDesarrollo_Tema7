6ac54cdb2454a91041ab2830220aadb2ec796abe
Isanvil <isaac.isanvil@gmail.com>
2024-04-23 13:04:17 +0200

    "Segundo commit. test del 1 al 3 passados"

--- tasca0/src/FizzBuzz.java
+++ tasca0/src/FizzBuzz.java
@@ -1,2 +1,21 @@
 public class FizzBuzz {
+    private int n;
+
+    public FizzBuzz(int n) {
+        this.n = n;
+    }
+
+    public String compruebaFizzBuzz(int n) {
+        String respuesta = "";
+        if (n % 3 == 0 && n % 5 == 0) {
+            return respuesta = "FizzBuzz";
+        } else if (n % 3 == 0) {
+            respuesta = "Fizz";
+        } else if (n % 5 == 0) {
+            respuesta = "Buzz";
+        } else {
+            respuesta = String.valueOf(n);
+        }
+        return respuesta;
+    }
 }
--- tasca0/src/FizzBuzzTest.java
+++ tasca0/src/FizzBuzzTest.java
@@ -1,5 +1,21 @@
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 class FizzBuzzTest {
+    @Test
+    public void testNum_1_2(){
+        FizzBuzz n = new FizzBuzz(1);
+        FizzBuzz n2 = new FizzBuzz(2);
+        assertEquals("1", n.compruebaFizzBuzz(1));
+        assertEquals("2", n2.compruebaFizzBuzz(2));
+    }
+
+    @Test
+    public void testNum_3(){
+        FizzBuzz n = new FizzBuzz(3);
+        assertEquals("Fizz", n.compruebaFizzBuzz(3));
+    }
 
 }
