de0143a25ba9768fb7b91e3cbad6996b1d1e6925
Isanvil <isaac.isanvil@gmail.com>
2024-04-23 13:47:55 +0200

    Tasca0B Segon Commit. Tests 3 i 4 passats

--- Tasca0B/src/OperacionesTDDTest.java
+++ Tasca0B/src/OperacionesTDDTest.java
@@ -14,4 +14,16 @@ class OperacionesTDDTest {
         OperacionesTDD op = new OperacionesTDD();
         assertEquals(1, op.suma("1"));
     }
+
+    @Test
+    public void test_1Mas2(){
+        OperacionesTDD op = new OperacionesTDD();
+        assertEquals(3, op.suma("1,2"));
+    }
+
+    @Test
+    public void test_1Mas1Mas2(){
+        OperacionesTDD op = new OperacionesTDD();
+        assertEquals(4, op.suma("1,1,2"));
+    }
 }
