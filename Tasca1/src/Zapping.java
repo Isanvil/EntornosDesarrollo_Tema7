d4fc2d174e82088f5597b9dd8dab93a21750087e
Isanvil <isaac.isanvil@gmail.com>
2024-04-30 12:45:59 +0200

    Tasca 1 - Commit 2 - Primer test passat

--- Tasca1/src/Zapping.java
+++ Tasca1/src/Zapping.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class Zapping {
+public class Zapping {
+    private final int MAX_CHANEL = 99;
+    private final int MIN_CHANEL = 0;
+
+    public Zapping() {
+
+    }
+
+    public int salida(int origen, int destino){
+        int salida = 0;
+
+        if (comprobarValidezRangoCanal(origen, destino)){
+            if(origen == destino){
+                salida = 0;
+            }
+        }
+
+        return salida;
+    }
+
+    public boolean comprobarValidezRangoCanal(int origen, int destino){
+        if (origen > MAX_CHANEL || origen < MIN_CHANEL || destino > MAX_CHANEL || destino < MIN_CHANEL){
+            return false;
+        }
+        return true;
+    }
+
 }
--- Tasca1/src/ZappingTest.java
+++ Tasca1/src/ZappingTest.java
@@ -1,4 +1,11 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class ZappingTest {
-  
+    @Test
+    public void testOrigenCeroDestinoCero(){
+        Zapping z = new Zapping();
+        assertEquals(0, z.salida(0, 0));
+    }
 }
