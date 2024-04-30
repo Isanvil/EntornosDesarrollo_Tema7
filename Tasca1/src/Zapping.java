ea85e4b221e7311dc37b250160d68f0eb1725513
Isanvil <isaac.isanvil@gmail.com>
2024-04-30 13:12:23 +0200

    Tasca 1 - Commit 4 - Tests 2 y 3 passats

--- Tasca1/src/Zapping.java
+++ Tasca1/src/Zapping.java
@@ -1,26 +1,35 @@
 public class Zapping {
     private final int MAX_CHANEL = 99;
-    private final int MIN_CHANEL = 0;
+    private final int MIN_CHANEL = 1;
 
     public Zapping() {
 
     }
 
     public int salida(int origen, int destino){
-        int salida = 0;
+        int salida = 1;
+        int canalMenor;
+        int canalMayor;
+
+        if (comprobarOrigenMayor(origen, destino)){
+            canalMenor = destino;
+            canalMayor = origen;
+        } else {
+            canalMenor = origen;
+            canalMayor = destino;
+        }
 
         if (comprobarValidezRangoCanal(origen, destino)){
             if(origen == destino){
                 salida = 0;
             } else {
-                if(comprobarOrigenMayor(origen, destino)){
-                    return origen - destino;
+                if (comprobarDiferenciaDe49(canalMenor, canalMayor)){
+                    salida = MAX_CHANEL - (canalMayor - canalMenor);
                 } else {
-                    return destino - origen;
+                    salida = canalMayor - canalMenor;
                 }
             }
         }
-
         return salida;
     }
 
@@ -33,6 +42,13 @@ public class Zapping {
 
     public boolean comprobarOrigenMayor(int origen, int destino){
         if (origen > destino){
+            return true;
+        }
+        return false;
+    }
+
+    public boolean comprobarDiferenciaDe49(int menor, int mayor){
+        if (mayor - menor == 49){
             return true;
         }
         return false;
--- Tasca1/src/ZappingTest.java
+++ Tasca1/src/ZappingTest.java
@@ -6,12 +6,24 @@ class ZappingTest {
     @Test
     public void testOrigenCeroDestinoCero(){
         Zapping z = new Zapping();
-        assertEquals(0, z.salida(0, 0));
+        assertEquals(0, z.salida(1, 1));
     }
 
     @Test
     public void testOrigenDosDestino1(){
         Zapping z = new Zapping();
         assertEquals(1, z.salida(2, 1));
+    }
+
+    @Test
+    public void testOrigenDiezDestinoTreinta(){
+        Zapping z = new Zapping();
+        assertEquals(20, z.salida(10, 30));
+    }
+
+    @Test
+    public void testOrigenDosDestino99(){
+        Zapping z = new Zapping();
+        assertEquals(20, z.salida(10, 30));
     }
 }
