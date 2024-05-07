5f9ff9086fde723e488ee2d79b684e21bfd02657
Isaac <isaac.isanvil@gmail.com>
2024-05-07 16:47:14 +0200

    Tasca 3 - Commit 8 Tests 14 al 16 passats

--- Tasca3/src/PasswordVerifyTest.java
+++ Tasca3/src/PasswordVerifyTest.java
@@ -147,17 +147,29 @@ class PasswordVerifyTest {
 
     @Test
     public void test_holacaracolaAExclamacion() {
+        PasswordVerify p = new PasswordVerify();
+        EsCorrecto fe = p.verificaContrasenya("holacaracolaA!");
 
+        assertEquals(false, fe.isCorrect());
+        assertEquals("La contrasenya ha de contenir almenys 2 números", fe.getMensaje());
     }
 
     @Test
     public void test_holacaracola12Exclamacion() {
+        PasswordVerify p = new PasswordVerify();
+        EsCorrecto fe = p.verificaContrasenya("holacaracola12!");
 
+        assertEquals(false, fe.isCorrect());
+        assertEquals("La contrasenya ha de contenir almenys una lletra majúscula", fe.getMensaje());
     }
 
     @Test
     public void test_holacaracola12A() {
+        PasswordVerify p = new PasswordVerify();
+        EsCorrecto fe = p.verificaContrasenya("holacaracola12A");
 
+        assertEquals(false, fe.isCorrect());
+        assertEquals("La contrasenya ha de contenir almenys un caràcter especial", fe.getMensaje());
     }
 
     @Test
